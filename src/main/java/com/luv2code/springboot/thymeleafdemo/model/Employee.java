b62615ac9f1877ce4b3060f5c7e7d7e1473868b9
admin <admin@admins-imac.gateway>
2022-10-27 20:27:45 +1100

    5th commit - CRUD Database - Get all employees from database

--- src/main/java/com/luv2code/springboot/thymeleafdemo/model/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/model/Employee.java
@@ -1,17 +1,34 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
+	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "email")
 	private String email;
 	
 	public Employee() {
 	}
-
-	public Employee(int id, String firstName, String lastName, String email) {
-		this.id = id;
+	
+	public Employee(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
@@ -40,7 +57,7 @@ public class Employee {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
@@ -53,4 +70,5 @@ public class Employee {
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
+	
 }
