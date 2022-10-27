24569b7daa1571d4daa3afb15b487e4a31b92a04
admin <admin@admins-imac.gateway>
2022-10-27 22:11:00 +1100

    8th commit - Add method to sort all employees by last name, asc

--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.model.Employee;
@@ -7,4 +9,10 @@ import com.luv2code.springboot.thymeleafdemo.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	// Add method to sort by lastName
+	// Spring Data JPA will parse the method name
+	// Looks for a specific format and pattern
+	// And create appropriate query ... behind the scenes
+	// read more at: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
